c00e094b8f26873eca9556f5092f4178f6d1fe58
Walker Silvestre <walker_contas@hotmail.com>
2025-12-17 23:42:19 -0300

    Aula 31

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -1,17 +1,23 @@
 extends CharacterBody2D
 
+const SPINNING_BONE = preload("res://entities/spinning_bone.tscn")
+
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var wall_detector_01: RayCast2D = $WallDetector01
 @onready var wall_detector_02: RayCast2D = $WallDetector02
 @onready var ground_detector: RayCast2D = $GroundDetector
+@onready var player_detector: RayCast2D = $PlayerDetector
 @onready var turn_timer: Timer = $TurnTimer
+@onready var bone_starting_position: Node2D = $BoneStartingPosition
 
 const SPEED = 10 * 100
 const JUMP_VELOCITY = -400.0
 var will_turn := false
+var can_throw := true
 
 enum SkeletonState {
+	attack,
 	walk,
 	dead,
 	idle,
@@ -29,6 +35,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	match curr_status:
+		SkeletonState.attack:
+			attack_state(delta)
 		SkeletonState.walk:
 			walk_state(delta)
 		SkeletonState.dead:
@@ -37,6 +45,12 @@ func _physics_process(delta: float) -> void:
 			idle_state(delta)
 	
 	move_and_slide()
+
+func go_to_attack_state():
+	curr_status = SkeletonState.attack
+	anim.play("attack")
+	velocity.x = 0
+	can_throw = true
 
 func go_to_walk_state():
 	curr_status = SkeletonState.walk
@@ -53,9 +67,19 @@ func go_to_idle_state():
 	curr_status = SkeletonState.idle
 	anim.play("idle")
 
+func attack_state(_delta: float):
+	if anim.frame == 2 and can_throw:
+		throw_bone()
+		can_throw = false
+		
+
 func walk_state(delta: float):
 	var direction := curr_direction.x
 	velocity.x = SPEED * direction * delta
+	
+	if must_attack():
+		go_to_attack_state()
+		return
 	
 	if must_turn():
 		update_raycasts()
@@ -63,13 +87,14 @@ func walk_state(delta: float):
 			will_turn = true
 			turn_timer.start()
 			go_to_idle_state()
-		
 
 func dead_state(_delta: float):
 	pass
 	
 func idle_state(_delta: float):
-	pass
+	if must_attack():
+		go_to_attack_state()
+		return
 
 func take_damage():
 	go_to_dead_state()
@@ -78,8 +103,20 @@ func turn_around():
 	curr_direction.x *= -1
 	scale.x *= -1
 
+func throw_bone():
+	var new_bone = SPINNING_BONE.instantiate()
+	
+	add_sibling(new_bone)
+	new_bone.position = bone_starting_position.global_position
+	
+	var direction := curr_direction.x
+	new_bone.set_direction(direction)
+
 func must_turn() -> bool:
 	return wall_detector_01.is_colliding() || wall_detector_02.is_colliding() || !ground_detector.is_colliding()
+
+func must_attack() -> bool:
+	return player_detector.is_colliding()
 	
 func update_raycasts():
 	wall_detector_01.force_raycast_update()
@@ -91,4 +128,8 @@ func _on_timer_timeout() -> void:
 		if will_turn:
 			turn_around()
 			will_turn = false
+		go_to_walk_state()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim.animation == "attack":
 		go_to_walk_state()
--- scripts/spinning_bone.gd
+++ scripts/spinning_bone.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
-var SPEED = 80
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+var SPEED = 120
 var curr_direction := Vector2.RIGHT
 
 func _process(delta: float) -> void:
 	position.x += SPEED * curr_direction.x * delta
+
+func set_direction(new_direction) -> void:
+	self.curr_direction = Vector2(new_direction, 0)
+	anim.flip_h = new_direction < 0
